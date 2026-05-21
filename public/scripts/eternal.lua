3485fd0ffddab18035654402ec1eb24cc4cb5145
bxshxxx <bse408519@gmail.com>
2026-05-21 10:49:18 -0400

    Update eternal.lua

--- public/scripts/eternal.lua
+++ public/scripts/eternal.lua
@@ -5,7 +5,7 @@
 -- ============================================================
 
 -- UI Library
-local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/kitodoescode/Bracket/main/BracketV34.lua"))()
+local ui = loadstring(game:HttpGet("https://cdn.bxsh.info/libs/eternalLIB.lua"))()
 
 -- Services
 local players    = game:GetService("Players")
